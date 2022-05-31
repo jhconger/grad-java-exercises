7f36ec54638af98ce38226ffef62d4eb8072e3ec
Justin Conger <jhconger@GMAIL.COM>
2022-05-31 18:17:46 -0500

    palindrome exercise

---
+++ src/com/company/Main.java
@@ -0,0 +1,97 @@
+package com.company;//package com.company;
+//
+//
+//import java.util.Arrays;
+//
+//public class Main {
+//  static int distClosestNumbers(int[] numbers) {
+//      System.out.println("The original array is: ");
+//      for (int num : numbers) {
+//          System.out.print(num + " ");
+//      }
+//      Arrays.sort(numbers);
+//      System.out.println("\nThe sorted array is: ");
+//      for (int num : numbers) {
+//          System.out.print(num + " ");
+//      }
+//      int l = 0;
+//      for (int i = 0; i < numbers.length; i++) {
+//          for (int k = i + 1; k < numbers.length; k++) {
+//              if (numbers[i] != numbers[k]) {
+//                  l = (numbers[k] - numbers[i]);
+//              }
+//          }
+//      }
+//////                        // try to implement it!
+//      return l;
+//  }
+//
+//
+//            public static void main(String[] args) {
+//                int[] testArray = {3, 9, 50, 15, 99, 7, 98, 65};
+//                int result = distClosestNumbers(testArray);
+//                System.out.println(result); // Expected result is 1 (the 2 closest numbers are 98 and 99)
+//            }
+//        }
+import java.util.*;
+
+public class Main {
+
+//    // Returns the distance between the two closest numbers.
+//    static int distClosestNumbers(int[] numbers) {
+//        Arrays.sort(numbers);
+//        int l = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            for (int k = i + 1; k < numbers.length; k++) {
+//                if (numbers[i] != numbers[k]) {
+//                    l = (numbers[k] - numbers[i]);
+//                }
+//
+//            }
+//        }
+//        return l;
+//    }
+//                public static void main(String[] args) {
+//                    int[] testArray = {3, 9, 50, 15, 99, 7, 98, 65};
+//                    int result = distClosestNumbers(testArray);
+//                    System.out.println(result); // Expected result is 1 (the 2 closest numbers are 98 and 99)
+//                }
+//            }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+ public static void main (String[] args) {
+     String str = "10001";
+     String str2 = "MoM";
+
+     str = str.toLowerCase();
+     str2 = str2.toLowerCase();
+
+     if (isPalindrome(str))
+         System.out.print("It is a palindrome");
+     else
+         System.out.print("It is not a palindrome");
+
+     // new line for better readability
+     System.out.println();
+
+     // For string 2
+     System.out.print("String 2 : ");
+     if (isPalindrome(str2))
+         System.out.print("It is a palindrome");
+     else
+         System.out.print("It is not a palindrome");
+ }
+}
+
+
