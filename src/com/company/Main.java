b36c78b2709c1a11244c67945806ca05dea93fd2
Justin Conger <jhconger@GMAIL.COM>
2022-06-03 13:37:34 -0500

    fizz buzz exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;//package com.company;
-//
-//
-//import java.util.Arrays;
-//
-//public class Main {
+import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.IntStream;
+
+import java.util.Arrays;
+
+public class Main {
 //  static int distClosestNumbers(int[] numbers) {
 //      System.out.println("The original array is: ");
 //      for (int num : numbers) {
@@ -33,9 +35,9 @@ package com.company;//package com.company;
 //                System.out.println(result); // Expected result is 1 (the 2 closest numbers are 98 and 99)
 //            }
 //        }
-import java.util.*;
 
-public class Main {
+
+//public class Main {
 
 //    // Returns the distance between the two closest numbers.
 //    static int distClosestNumbers(int[] numbers) {
@@ -57,41 +59,55 @@ public class Main {
 //                    System.out.println(result); // Expected result is 1 (the 2 closest numbers are 98 and 99)
 //                }
 //            }
+//
+//    public static boolean isPalindrome(String str) {
+//        int i = 0, j = str.length() - 1;
+//        while (i < j) {
+//            if (str.charAt(i) != str.charAt(j))
+//                return false;
+//            i++;
+//            j--;
+//        }
+//        return true;
+//    }
+//
+//
+// public static void main (String[] args) {
+//     String str = "10001";
+//     String str2 = "MoM";
+//
+//     str = str.toLowerCase();
+//     str2 = str2.toLowerCase();
+//
+//     if (isPalindrome(str))
+//         System.out.print("It is a palindrome");
+//     else
+//         System.out.print("It is not a palindrome");
+//
+//     // new line for better readability
+//     System.out.println();
+//
+//     // For string 2
+//     System.out.print("String 2 : ");
+//     if (isPalindrome(str2))
+//         System.out.print("It is a palindrome");
+//     else
+//         System.out.print("It is not a palindrome");
+// }
+//}
 
-    public static boolean isPalindrome(String str) {
-        int i = 0, j = str.length() - 1;
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
-                return false;
-            i++;
-            j--;
+
+
+
+        public static void main(String[] args)
+        {
+            fizzBuzzBeforeJava8(100);
+
         }
-        return true;
-    }
 
+        private static void fizzBuzzBeforeJava8(int num)
+        {
+            for(int i=0;++i<101;)
+                System.out.println(i%3>0?i%5>0?i:"Buzz":i%5>0?"Fizz":"FizzBuzz");}}
 
- public static void main (String[] args) {
-     String str = "10001";
-     String str2 = "MoM";
-
-     str = str.toLowerCase();
-     str2 = str2.toLowerCase();
-
-     if (isPalindrome(str))
-         System.out.print("It is a palindrome");
-     else
-         System.out.print("It is not a palindrome");
-
-     // new line for better readability
-     System.out.println();
-
-     // For string 2
-     System.out.print("String 2 : ");
-     if (isPalindrome(str2))
-         System.out.print("It is a palindrome");
-     else
-         System.out.print("It is not a palindrome");
- }
-}
-
-
+//            for (var i = 1; i <= 100; i++) { var result = “”; if (i % 3 === 0) { result = “Fizz”; } if (i % 5 === 0) { result += “Buzz”; } console.log(result || i); }
