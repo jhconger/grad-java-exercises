2a015e585ed1cba48c632f8dd51b90b01a17a3b2
jhconger <jhconger@gmail.com>
2022-07-07 00:41:42 -0500

    comments for ternary truthy falsy FizzBuzz solution.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;//package com.company;
+
 import java.util.stream.IntStream;
 import java.util.*;
 import java.util.stream.IntStream;
@@ -99,19 +100,23 @@ public class Main {
 //  for (var i = 1; i <= 100; i++) { var result = “”; if (i % 3 === 0) { result = “Fizz”; } if (i % 5 === 0) { result += “Buzz”; } console.log(result || i); }
 
 
-//  best solution I have found thus far
+    //  best solution I have found thus far
     public static void main(String[] args) {
 
-            for (int i = 0; i++ < 101; )
-//    iterate from 1-100
-
-                System.out.println(i % 3 > 0 ? i % 5 > 0 ? i : "Buzz" : i % 5 > 0 ? "Fizz" : "FizzBuzz");
-//     if Argument A (i % 3 > 0) evaluates to true  THEN test Argument B (i % 5 > 0)
-//     if Argument A (i % 3 > 0) evaluates to false THEN print Fizz
-//     if Argument B (i % 5 > 0) evaluates to true  THEN print Buzz
-//     and remainder greater than zero THEN if i is divisible by 5 and remainder greater than zero then print i.    print Buzz
-        }
+        for (int i = 0; i++ < 100; )
+            System.out.println(i % 3 > 0 ? i % 5 > 0 ? i : "Buzz" : i % 5 > 0 ? "Fizz" : "FizzBuzz");
     }
+}
+
+//     is i divisible by 3
+//              YES: test i divisible by 5
+//                            YES: print FizzBuzz
+//                            NO: print Fizz
+//
+//              NO: test i divisible by 5
+//                            YES: print Buzz
+//                            NO: print i
+
 
 //    public static void main(String[] args) {
 //
