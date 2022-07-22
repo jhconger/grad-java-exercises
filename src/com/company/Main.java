841c805e87892cff45f135884cb8b83da465047b
jhconger <jhconger@gmail.com>
2022-07-21 19:29:12 -0500

    print various shapes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
+
 //
 public class Main {
 //  static int distClosestNumbers(int[] numbers) {
@@ -416,3 +417,102 @@ public class Main {
 //    }
 //}
 //
+
+// print following shapes:
+//        $$
+//       $$$$
+//      $$$$$$
+//     $$$$$$$$
+//    $$$$$$$$$$
+//   $$$$$$$$$$$$
+//               #
+//              #  #
+//             #    #
+//            #      #
+//           #        #
+//          #          #
+//         #            #
+//         # # # # # # # #
+//        #
+//      #  #
+//     #    #
+//    #      #
+//     #    #
+//      #  #
+//       #
+
+
+//    public static void main(String[] args) {
+//        for (int i = 6; i >= 1; i--) {
+//            for (int j = 1; j < i; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 6; k >= i; k--) {
+//                System.out.print("$");
+//            }
+//            for (int k = 6; k >= i; k--) {
+//                System.out.print("$");
+//            }
+//            System.out.println();
+//        }
+
+
+//
+//    public static void main(String[] args) {
+//        System.out.println("      #");
+//                for (int i = 6; i >= 1; i--) {
+//
+//            for (int j = 1; j < i; j++) {
+//                System.out.print(" ");
+//            }
+//                    System.out.print("#");
+//            for (int k = 6; k >= i; k--) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 6; k >= i; k--) {
+//                System.out.print(" ");
+//            }
+//                    System.out.print("#");
+//                    System.out.println();
+//        }
+//                for( int k = 7; k>= 0; k--) {
+//                    System.out.print("# ");
+//                }
+//
+public static void main(String[] args) {
+    System.out.println("    #");
+                for (int i = 3; i >= 1; i--) {
+
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+                    System.out.print("#");
+            for (int k = 3; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int k = 3; k >= i; k--) {
+                System.out.print(" ");
+            }
+                    System.out.print("#");
+                    System.out.println();
+        }
+
+    for (int i = 2; i >= 1; i--) {
+
+        for (int j = 2; j >= i; j--) {
+            System.out.print(" ");
+        }
+        System.out.print("#");
+        for (int k = 0; k < i; k++) {
+            System.out.print(" ");
+        }
+        for (int k = 0; k < i; k++) {
+            System.out.print(" ");
+        }
+        System.out.print("#");
+        System.out.println();
+    }
+    System.out.println("   #");
+    }
+}
+
