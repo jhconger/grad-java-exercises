dd359c2374c11244d024571b2dcda90743f38235
jhconger <jhconger@gmail.com>
2022-08-05 20:20:06 -0500

    more java array exercises

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -645,17 +645,36 @@ public class Main {
 //}
 
 
-
-//declare and initialize two arrays and print numbers that are in both arrays.
+    //declare and initialize two arrays and print numbers that are in both arrays.
+// then take values of arrays and swap them. (Arr1 has values of arr2 and vice versa.
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 13, 4, 5, 6, 17, 8, 9, 10};
         int[] arr2 = {11, 12, 13, 2, 15, 16, 17, 9, 19, 20};
+
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1.length; j++) {
                 if (arr1[i] == arr2[j]) {
                     System.out.println(arr1[i]);
                 }
             }
+        }
+        int[] arr3 = new int[10];
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i];
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr2[i];
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr3[i];
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr3[i];
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
         }
     }
 }
